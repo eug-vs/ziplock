ae8c1f02a7f78a71863e564879d5b24843113635
eug-vs <eug-vs@keemail.me>
2020-12-10 22:54:48 +0300

    build: name the container

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker build . --tag ziplock
-docker run -it -v $(pwd):/home/developer/ziplock -v /dev/bus/usb:/dev/bus/usb --privileged ziplock:latest
+docker run -it -v $(pwd):/home/developer/ziplock -v /dev/bus/usb:/dev/bus/usb --privileged --name ziplock-interactive ziplock:latest
